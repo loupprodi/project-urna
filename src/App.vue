957fc1fd3698d44ea3d9c3811311b7004fe1382a
Luiz Felipe V Prosdoskimi <82850974+loupprodi@users.noreply.github.com>
2023-03-30 21:04:51 -0300

    first commit
    urn design using vue.js through WEBDEV class

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="urna"></div>
+  </div>
+</template>
+
+<script>
+import '@/css/global.css';
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: var(--background-color);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.urna{
+  width: 100%;  
+}
+</style>
